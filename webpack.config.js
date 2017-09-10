1f16cfed62c7147041ecdc16090e77f9cbaaac3e
APZelos <a.p.zelos@gmail.com>
2017-09-10 16:30:23 +0300

    Added ExtractTextPlugin to webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
 const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
-  entry: './src/toastify.js',
-  output: {
-    filename: 'toastify.js',
-    path: path.resolve(__dirname, 'dist')
-  }
+    entry: './src/toastify.js',
+    output: {
+        filename: 'toastify.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [{
+            test: /\.scss$/,
+            use: ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: 'sass-loader'
+            })
+        }]
+    },
+    plugins: [
+      new ExtractTextPlugin('toastify.css'),
+    ]
 };
